bb1c0b339a28fd3255ce6ed6f13afb8707f5ecd2
erkanerten <erkanerten1999@gmail.com>
2022-05-24 16:13:27 +0300

    UI UPDATE
    UI 1.5 Update; Level Lock System is ready.

---
+++ Deep Ocean/Assets/GameFolder/Scripts/Concretes/Menu/Menu/LevelManager.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using UnityEngine.EventSystems;
+
+
+public class LevelManager : MonoBehaviour
+{
+
+    public  List<Button> levelButton;
+
+    public bool delete;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (delete)
+            PlayerPrefs.DeleteAll();
+        int saveIndex = PlayerPrefs.GetInt("SaveIndex");
+
+        for (int i = 0; i < levelButton.Count; i++)
+        {
+            if (i<=saveIndex)
+            {
+                levelButton[i].interactable = true;
+            }
+            else
+            {
+                levelButton[i].interactable = false;
+            }
+        }
+        
+    }
+
+    public void LevelSelect()
+    {
+        int level = int.Parse(EventSystem.current.currentSelectedGameObject.name);
+        SceneManager.LoadScene(level+1);
+    }
+}
---
+++ Deep Ocean/Assets/GameFolder/Scripts/Concretes/Menu/Menu/NextLevelManager.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class NextLevelManager : MonoBehaviour
+{
+    public int buildIndex = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        buildIndex = SceneManager.GetActiveScene().buildIndex;
+        Text levelText = GameObject.Find("LevelText").GetComponent<Text>();
+        levelText.text = "Level_" + (buildIndex-1).ToString();
+    }
+
+    public void NextLevel()
+    {
+        int saveIndex = PlayerPrefs.GetInt("SaveIndex");
+        if (buildIndex>saveIndex)
+        {
+            PlayerPrefs.SetInt("SaveIndex",buildIndex);
+        }
+        if(buildIndex == 6)
+        {
+            SceneManager.LoadScene(0);
+        }
+        else
+        {
+            SceneManager.LoadScene(buildIndex + 1);
+        }
+    }
+
+
+}
